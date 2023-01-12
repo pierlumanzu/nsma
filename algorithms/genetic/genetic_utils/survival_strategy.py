7bd874af4e2c8b280fa1488b685039053b83efb5
mansueto <mansueto@webgol.dinfo.unifi.it>
2023-01-12 11:05:22 +0100

    Survival strategies fixed

--- algorithms/genetic/genetic_utils/survival_strategy.py
+++ algorithms/genetic/genetic_utils/survival_strategy.py
@@ -101,6 +101,11 @@ class GeneticSurvivalStrategy:
                 For each survived infeasible solution, the associated rank and crowding distance are equal to infinity
         """
 
+        index_not_nan_f = ~np.isnan(np.sum(f_list, axis=1))
+        p_list = p_list[index_not_nan_f, :]
+        f_list = f_list[index_not_nan_f, :]
+        constraint_violations = constraint_violations[index_not_nan_f]
+
         n_survive = min(self._pop_size, len(p_list))
 
         feasible, infeasible = split_by_feasibility(constraint_violations)
